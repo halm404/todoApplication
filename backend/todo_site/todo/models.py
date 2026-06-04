b873bf878d3be86252384e746c73934a6c3cb3b9
himalm <himalm@abo.fi>
2026-06-04 11:32:22 +0300

    Add list model

--- backend/todo_site/todo/models.py
+++ backend/todo_site/todo/models.py
@@ -1,11 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='lists'
+    )
+
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(default='')
     deadline = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=20, default='')
 
+    todo_list = models.ForeignKey(
+        TodoList,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        default=''
+    )
+
     def __str__(self):
         return self.title
+
